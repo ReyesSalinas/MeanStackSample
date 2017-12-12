b65535d95a77fba047e40903507822459bdfd2f5
Reyes Salinas <kingsofsalt@gmail.com>
2017-12-12 09:52:55 -0600

    ++first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs/Observable";
+import {of} from "rxjs/observable/of";
+import {User} from "./user";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = "City Rats";
+  users;
+  Username;
+  Password;
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+      this.getUsers();
+
+  }
+
+  getUsers() {
+    this.http.get("https://api.github.com/users").subscribe(data => {
+      this.users = data;
+    });
+
+  }
+
+  onUserInput(value) {
+    this.Username = value;
+  }
+  signIn() {
+
+    for (let i = 0, len = this.users.length; i < len; i++) {
+      if (this.users[i].login === this.Username ) {
+        alert("You have logged in!");
+        return;
+      }
+    }
+    alert("Invalid Username or Password!");
+  }
 }
